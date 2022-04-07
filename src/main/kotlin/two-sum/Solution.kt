eefdf8774d99f4c156813b7e0331aeb625c71142
Aaron Lin <aaron.lin@ringcentral.com>
2022-04-07 18:34:38 +0800

    AC https://leetcode-cn.com/problems/two-sum/

---
+++ src/main/kotlin/two-sum/Solution.kt
@@ -0,0 +1,17 @@
+package `two-sum`
+
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val map = hashMapOf<Int,Int>()
+        nums.forEachIndexed { index, i ->
+            val k = target - i
+            if (map.containsKey(k)){
+                return intArrayOf(map[k]!!, index)
+            }else{
+                map[i] = index
+            }
+        }
+        return intArrayOf(0)
+    }
+
+}
