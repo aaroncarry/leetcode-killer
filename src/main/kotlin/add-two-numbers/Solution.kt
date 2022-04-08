97acdd1fe819d97ef288b2b264bd7783e0228ce6
Aaron Lin <aaron.lin@ringcentral.com>
2022-04-08 21:40:10 +0800

    AC https://leetcode-cn.com/problems/add-two-numbers/

---
+++ src/main/kotlin/add-two-numbers/Solution.kt
@@ -0,0 +1,41 @@
+package `add-two-numbers`
+
+import ListNode
+import kotlin.math.pow
+
+class Solution {
+    fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
+        var head: ListNode? = null
+        var mergeNode: ListNode? = null
+        var increase = 0
+        var p1 = l1
+        var p2 = l2
+        while (p1 != null || p2 != null) {
+            var v1 = 0
+            var v2 = 0
+            if (p1 != null) {
+                v1 = p1.`val`
+                p1 = p1.next
+            }
+            if (p2 != null) {
+                v2 = p2.`val`
+                p2 = p2.next
+            }
+            var sum = v1 + v2 + increase
+            increase = sum / 10
+            sum %= 10
+            if (head == null) {
+                head = ListNode(sum)
+                mergeNode = head
+            } else {
+                mergeNode?.next = ListNode(sum)
+                mergeNode = mergeNode?.next
+            }
+        }
+        if (increase != 0) {
+            mergeNode?.next = ListNode(increase)
+        }
+        return head
+    }
+
+}
