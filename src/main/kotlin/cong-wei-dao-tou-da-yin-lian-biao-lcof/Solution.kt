fd7771ebc24d7f36d8c497b08436d68eda844cef
Aaron Lin <aaron.lin@ringcentral.com>
2022-04-08 22:01:22 +0800

    AC https://leetcode-cn.com/problems/cong-wei-dao-tou-da-yin-lian-biao-lcof/submissions/

---
+++ src/main/kotlin/cong-wei-dao-tou-da-yin-lian-biao-lcof/Solution.kt
@@ -0,0 +1,15 @@
+package `cong-wei-dao-tou-da-yin-lian-biao-lcof`
+
+import ListNode
+
+class Solution {
+    fun reversePrint(head: ListNode?): IntArray {
+        if (head == null) return intArrayOf()
+        return recPrint(head)
+    }
+
+    fun recPrint(node: ListNode): IntArray {
+        if (node.next == null) return intArrayOf(node.`val`)
+        return reversePrint(node.next).plus(node.`val`)
+    }
+}
