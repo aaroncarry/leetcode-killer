2ae74ef439004c93d37e74e3fd14877ffe8a2a97
Aaron Lin <aaron.lin@ringcentral.com>
2022-04-08 21:56:43 +0800

    AC https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof/

---
+++ src/main/kotlin/ti-huan-kong-ge-lcof/Solution.kt
@@ -0,0 +1,9 @@
+package `ti-huan-kong-ge-lcof`
+
+//https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof/
+class Solution {
+    //这题有点脱裤子放屁的感觉，懒得做，最优做法应该是先计算出空格数，然后算出需要的空间数，从后到前移动字符串，时间复杂度n
+    fun replaceSpace(s: String): String {
+        return s.replace(" ","%20")
+    }
+}
