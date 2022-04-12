b275b865517de5d4d923828eec44dacc73cef09c
Aaron Lin <aaron.lin@ringcentral.com>
2022-04-12 22:07:22 +0800

    AC https://leetcode-cn.com/problems/remove-k-digits/

--- src/main/kotlin/leetcode/remove-k-digits/Solution.kt
+++ src/main/kotlin/leetcode/remove-k-digits/Solution.kt
@@ -1,22 +1,28 @@
 package leetcode.`remove-k-digits`
 
 import org.junit.Test
+import java.util.Stack
 
 class Solution {
     fun removeKdigits(num: String, k: Int): String {
         if (num.length <= k) return "0"
         var processString = num
+        val stack = Stack<Char>()
         repeat(k) {
-            processString = greedy(processString)
+            processString = greedy(processString, stack)
         }
         return processString.dropWhile { it == '0' }.ifEmpty { "0" }
     }
 
-    fun greedy(num: String): String {
+    fun greedy(num: String, stack: Stack<Char>): String {
         var index = -1
-        for (i in 1..num.lastIndex) {
-            if (num[i] < num[i - 1]) {
+
+        for (i in stack.size..num.lastIndex) {
+            if (stack.isEmpty() || num[i] >= stack.peek()) {
+                stack.push(num[i])
+            } else {
                 index = i - 1
+                stack.pop()
                 break
             }
         }
@@ -29,7 +35,7 @@ class Solution {
 
     @Test
     fun test() {
-//        removeKdigits("1432219", 3)
+        removeKdigits("1432219", 3)
         removeKdigits("112", 1)
     }
 }
